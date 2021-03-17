2b20e9b541061b2fcf9348d01b6a65846940eba1
ivanviso <ivanviso123@gmail.com>
2021-03-17 23:22:40 +0100

    final creo

--- bind9.py
+++ bind9.py
@@ -12,7 +12,7 @@ for host in hosts:
 
 
 def bindhost(host):
-    zone='IN { type master; notify no; file "null.zone.file"; };'
+    zone='IN { type master; notify no; file "/etc/named/null.zone.file"; };'
     host="zone \""+host+"\" "
     return host+zone
 
@@ -36,5 +36,22 @@ with open('/etc/squid/squid_blackhole.conf', 'w') as f:
     for host in squidhostlist:
         f.write(host+"\n")
     f.close
+with open('/etc/named/null.zone.file', 'w') as f:
+    f.write("""; BIND db file for ad servers - point all addresses to an invalid IP
+$TTL	864000	; ten days
+
+@       IN      SOA     ns0.example.net.      hostmaster.example.net. (
+                        2008032800       ; serial number YYMMDDNN
+                        288000   ; refresh  80 hours
+                        72000    ; retry    20 hours
+                        8640000  ; expire  100 days
+                        864000 ) ; min ttl  10 day
+                NS      ns0.example.net.
+
+		A	0.0.0.0
+
+*		IN      A       0.0.0.0""")
+    f.close
+
 os.system('rndc reload')
 os.system('squid -k reconfigure')
